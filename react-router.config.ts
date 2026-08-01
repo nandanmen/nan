c73b0e2d3d16e2b97033fe78cf80b4f7a25eb0ce
Nanda Syahrasyad <nanda.s@hey.com>
2026-08-01 10:34:18 -0700

    fix: make work as a subpath

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  basename: "/p/",
   ssr: true,
   future: {
     unstable_viteEnvironmentApi: true,
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/p/",
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
