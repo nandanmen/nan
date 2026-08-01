f37c3c898be361fbcec91d460173eb663a617c41
Nanda Syahrasyad <nanda.s@hey.com>
2026-08-01 13:07:37 -0700

    chore: simplify + remove index route

--- app/root.tsx
+++ app/root.tsx
@@ -2,22 +2,22 @@ import { Links, Meta, Outlet, Scripts } from "react-router";
 import "./app.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta />
+				<Links />
+			</head>
+			<body className="antialiased">
+				{children}
+				<Scripts />
+			</body>
+		</html>
+	);
 }
 
 export default function App() {
-  return <Outlet />;
+	return <Outlet />;
 }
