238c5b5158def2b30ef7d52c441b51ebedfd66b3
Nanda Syahrasyad <nanda.s@hey.com>
2026-08-01 11:11:21 -0700

    fix: manually fetch assets

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/p/",
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
--- workers/app.ts
+++ workers/app.ts
@@ -16,6 +16,12 @@ const requestHandler = createRequestHandler(
 
 export default {
   fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/p/assets/")) {
+      url.pathname = url.pathname.slice(2);
+      return env.ASSETS.fetch(new Request(url, request));
+    }
+
     return requestHandler(request, {
       cloudflare: { env, ctx },
     });
