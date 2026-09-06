5fbf1c2cd2d2c9a98a1c3ed920ebb10d2b08e2cd
Nanda Syahrasyad <nanda.s@hey.com>
2026-09-06 10:32:18 -0700

    feat: simplify button

--- app/routes/internet/components/little-internet-button.tsx
+++ app/routes/internet/components/little-internet-button.tsx
@@ -1,41 +1,46 @@
-import type { ComponentProps } from "react";
-import { cn } from "cn";
 import {
   useScrollerCanSend,
   useScrollerDispatch,
-  type ScrollerEvent,
 } from "../../../components/scroller";
+import { motion } from "motion/react";
 
-type LittleInternetButtonProps = ComponentProps<"button"> & {
-  event: ScrollerEvent;
-};
-
-export function LittleInternetButton({
-  event,
-  children,
-  className,
-  disabled,
-  onClick,
-  ...props
-}: LittleInternetButtonProps) {
+export function LittleInternetButton() {
   const dispatch = useScrollerDispatch();
-  const canSend = useScrollerCanSend(event);
+
+  const canSendAdd = useScrollerCanSend({ type: "add" });
+  const canSendReset = useScrollerCanSend({ type: "reset" });
 
   return (
-    <button
-      {...props}
-      type="button"
-      className={cn(
-        "w-fit rounded bg-gray-12 border-t-2 border-gray-11 px-4 py-2 text-gray-1 font-medium cursor-pointer hover:bg-gray-11 focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-gray-12 disabled:opacity-50 disabled:cursor-default",
-        className,
-      )}
-      disabled={disabled || !canSend}
-      onClick={(eventObject) => {
-        onClick?.(eventObject);
-        if (!disabled && canSend && !eventObject.defaultPrevented) dispatch(event);
-      }}
-    >
-      {children}
-    </button>
+    <div className="h-10 bg-white flex divide-x divide-black/10 w-fit ring ring-black/10 shadow rounded overflow-hidden">
+      <motion.button
+        className="h-full flex items-center border-b-2 border-gray-4 px-3 not-disabled:hover:bg-gray-2 disabled:cursor-not-allowed disabled:text-gray-8"
+        disabled={!canSendAdd}
+        onClick={() => dispatch({ type: "add" })}
+      >
+        Add a computer
+      </motion.button>
+      <button
+        className="h-full aspect-square flex items-center justify-center border-b-2 border-gray-4 not-disabled:hover:bg-gray-2 disabled:cursor-not-allowed disabled:text-gray-8"
+        disabled={!canSendReset}
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        <span className="sr-only">Reset</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M19.2617 20.25V16.25H15.2617" />
+          <path d="M4.75 3.75V7.75H8.75" />
+          <path d="M3.81383 10.9688C3.7717 11.3066 3.75 11.6508 3.75 12C3.75 16.5563 7.44365 20.25 12 20.25C14.6766 20.25 17.1111 18.9754 18.6322 17" />
+          <path d="M20.186 13.0312C20.2281 12.6934 20.2498 12.3492 20.2498 12C20.2498 7.44365 16.5562 3.75 11.9998 3.75C9.32326 3.75 6.88871 5.02463 5.36768 7" />
+        </svg>
+      </button>
+    </div>
   );
 }
